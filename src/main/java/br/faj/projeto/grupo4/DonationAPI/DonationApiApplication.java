eb9f8423a20408e182b2a33d0cd0fbe2ef861c24
trutadez <gabriel.moises1@outlook.com>
2020-05-13 23:09:39 -0300

    projeto criado

---
+++ src/main/java/br/faj/projeto/grupo4/DonationAPI/DonationApiApplication.java
@@ -0,0 +1,13 @@
+package br.faj.projeto.grupo4.DonationAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/faj/projeto/grupo4/DonationAPI/DonationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.faj.projeto.grupo4.DonationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
